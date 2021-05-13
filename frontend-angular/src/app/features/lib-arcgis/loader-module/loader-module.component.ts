efe26024969cc92ad41fe6617ec8ae8a2d6e57cd
MICHEL Alexis <michel.alexis@outlook.com>
2021-05-13 22:00:11 +0200

    feat: Add dynamique container building

--- frontend-angular/src/app/features/lib-arcgis/loader-module/loader-module.component.ts
+++ frontend-angular/src/app/features/lib-arcgis/loader-module/loader-module.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 import { loadModules } from 'esri-loader';
 import esri = __esri;
@@ -6,7 +6,8 @@ import esri = __esri;
 @Component({
   selector: 'app-lib-arcgis-loader-module',
   templateUrl: './loader-module.component.html',
-  styleUrls: ['./loader-module.component.scss']
+  styleUrls: ['./loader-module.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoaderModuleComponent implements OnInit {
 
@@ -15,14 +16,23 @@ export class LoaderModuleComponent implements OnInit {
   }
 
   @ViewChild('mapAnchor', { static: true }) private mapAnchor!: ElementRef;
+  @ViewChild('searchAnchor', { static: true }) private searchAnchor!: ElementRef;
+  @ViewChild('layerListAnchor', { static: false }) private layerListAnchor!: ElementRef;
 
   private mapView!: esri.MapView;
   private map!: esri.Map;
 
+  private baseampGallery: esri.BasemapGallery;
+  private search: esri.widgetsSearch;
+  private layerList: esri.LayerList;
+
   private center!: esri.Point;
 
-  constructor() {
-  }
+  public showLayerList = false;
+
+  constructor(
+    private cdr: ChangeDetectorRef
+  ) {}
 
   async ngOnInit(): Promise<void> {
     const [Map, MapView, Point, projection] = await loadModules<[esri.MapConstructor, esri.MapViewConstructor, esri.PointConstructor, esri.projection]>([
@@ -67,6 +77,59 @@ export class LoaderModuleComponent implements OnInit {
       center: this.center,
       zoom: 8
     });
+  }
+
+  public async toggleBasemapGallery(): Promise<void> {
+    const [BasemapGallery] = await loadModules<[esri.BasemapGalleryConstructor]>(['esri/widgets/BasemapGallery']);
+
+    this.baseampGallery = new BasemapGallery({
+      view: this.mapView
+    });
+
+    this.mapView.ui.add(this.baseampGallery, {
+      position: 'top-right'
+    });
+  }
+
+  public async toggleSearch(): Promise<void> {
+    if(!this.search) {
+      const [Search] = await loadModules<[esri.widgetsSearchConstructor]>(['esri/widgets/Search']);
+
+      const container = document.createElement('div');
+      container.className = 'full'
+
+      this.searchAnchor.nativeElement.append(container);
+      this.search = new Search({
+        view: this.mapView,
+        container: container,
+      });
+    } else {
+      // Destroy method will remove the anchor from de DOM.
+      // If we give the "searchAnchor" to the widget, it cannot be used the second time.
+      this.search.destroy();
+      this.search = null;
+    }
+  }
+
+  public async toggleLayerList(): Promise<void> {
+    if(!this.layerList) {
+      this.showLayerList = true;
+      this.cdr.detectChanges();
+      const [LayerList] = await loadModules<[esri.LayerListConstructor]>(['esri/widgets/LayerList']);
+
+      const container = document.createElement('div');
+      container.className = 'full'
+
+      this.layerListAnchor.nativeElement.append(container);
+      this.layerList = new LayerList({
+        view: this.mapView,
+        container: container
+      })
+    } else {
+      this.layerList.destroy();
+      this.layerList = null;
+      this.showLayerList = false;
+    }
   }
 
 }
--- frontend-angular/src/app/features/lib-arcgis/loader-module/loader-module.module.ts
+++ frontend-angular/src/app/features/lib-arcgis/loader-module/loader-module.module.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LoaderModuleComponent } from './loader-module.component';
 
 @NgModule({
-  imports: [RouterModule.forChild([
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
     { path: '', component: LoaderModuleComponent },
   ])],
   declarations: [LoaderModuleComponent],
